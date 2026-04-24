465537c000ae331b1e07672d84c3c8aaf9e2f0c6
geazihenrique <geazi.henrique@gmail.com>
2026-04-24 20:34:07 -0300

    Reestrutura app para funcionar no GitHub Pages

--- js/gerarDXF.js
+++ js/gerarDXF.js
@@ -1,102 +1,97 @@
-(function () {
-  function formatNumber(value) {
-    return Number(value).toFixed(3).replace(/\.?0+$/, "");
-  }
+function formatNumber(value) {
+  return Number(value).toFixed(3).replace(/\.?0+$/, "");
+}
 
-  function rectangleLines(x, y, width, height) {
-    return [
-      [x, y, x + width, y],
-      [x + width, y, x + width, y + height],
-      [x + width, y + height, x, y + height],
-      [x, y + height, x, y],
-    ];
-  }
+function createRectangleEntities(x, y, width, height) {
+  return [
+    [x, y, x + width, y],
+    [x + width, y, x + width, y + height],
+    [x + width, y + height, x, y + height],
+    [x, y + height, x, y],
+  ];
+}
 
-  function gerarDXF(parts) {
-    const spacing = 20;
-    let offsetX = 0;
-    const entities = [];
+export function gerarDXF(pecas) {
+  const spacing = 20;
+  let offsetX = 0;
+  const entities = [];
 
-    parts.forEach((piece) => {
-      for (let i = 0; i < piece.quantidade; i += 1) {
-        rectangleLines(offsetX, 0, piece.largura, piece.alturaProfundidade).forEach((line) => {
-          entities.push(
-            "0",
-            "LINE",
-            "8",
-            "CORTE",
-            "10",
-            formatNumber(line[0]),
-            "20",
-            formatNumber(line[1]),
-            "30",
-            "0",
-            "11",
-            formatNumber(line[2]),
-            "21",
-            formatNumber(line[3]),
-            "31",
-            "0"
-          );
-        });
+  pecas.forEach((peca) => {
+    for (let index = 0; index < peca.quantidade; index += 1) {
+      const linhas = createRectangleEntities(offsetX, 0, peca.largura, peca.alturaProfundidade);
 
-        offsetX += piece.largura + spacing;
-      }
-    });
+      linhas.forEach((linha) => {
+        entities.push(
+          "0",
+          "LINE",
+          "8",
+          "CORTE",
+          "10",
+          formatNumber(linha[0]),
+          "20",
+          formatNumber(linha[1]),
+          "30",
+          "0",
+          "11",
+          formatNumber(linha[2]),
+          "21",
+          formatNumber(linha[3]),
+          "31",
+          "0"
+        );
+      });
 
-    return [
-      "0",
-      "SECTION",
-      "2",
-      "HEADER",
-      "9",
-      "$INSUNITS",
-      "70",
-      "4",
-      "0",
-      "ENDSEC",
-      "0",
-      "SECTION",
-      "2",
-      "TABLES",
-      "0",
-      "TABLE",
-      "2",
-      "LAYER",
-      "70",
-      "1",
-      "0",
-      "LAYER",
-      "2",
-      "CORTE",
-      "70",
-      "0",
-      "62",
-      "7",
-      "6",
-      "CONTINUOUS",
-      "0",
-      "ENDTAB",
-      "0",
-      "ENDSEC",
-      "0",
-      "SECTION",
-      "2",
-      "ENTITIES",
-      ...entities,
-      "0",
-      "ENDSEC",
-      "0",
-      "EOF",
-    ].join("\n");
-  }
+      offsetX += peca.largura + spacing;
+    }
+  });
 
-  function getDXFFileName(calculo) {
-    return `cupula_${Math.round(calculo.external.largura)}x${Math.round(calculo.external.profundidade)}x${Math.round(calculo.external.altura)}_${Math.round(calculo.espessura)}mm.dxf`;
-  }
+  return [
+    "0",
+    "SECTION",
+    "2",
+    "HEADER",
+    "9",
+    "$INSUNITS",
+    "70",
+    "4",
+    "0",
+    "ENDSEC",
+    "0",
+    "SECTION",
+    "2",
+    "TABLES",
+    "0",
+    "TABLE",
+    "2",
+    "LAYER",
+    "70",
+    "1",
+    "0",
+    "LAYER",
+    "2",
+    "CORTE",
+    "70",
+    "0",
+    "62",
+    "7",
+    "6",
+    "CONTINUOUS",
+    "0",
+    "ENDTAB",
+    "0",
+    "ENDSEC",
+    "0",
+    "SECTION",
+    "2",
+    "ENTITIES",
+    ...entities,
+    "0",
+    "ENDSEC",
+    "0",
+    "EOF",
+  ].join("\n");
+}
 
-  window.CupulaDXF = {
-    gerarDXF,
-    getDXFFileName,
-  };
-})();
+export function getDXFFileName(calculo) {
+  return `cupula_${Math.round(calculo.medidasExternas.largura)}x${Math.round(calculo.medidasExternas.profundidade)}x${Math.round(calculo.medidasExternas.altura)}_${Math.round(calculo.configuracao.espessura)}mm.dxf`;
+}
