deb49ae4a429f50b9b50f29b446a04cb5c5660e2
geazihenrique <geazi.henrique@gmail.com>
2026-04-24 20:33:46 -0300

    Reestrutura app para funcionar no GitHub Pages

--- js/state.js
+++ js/state.js
@@ -1,30 +1,14 @@
-(function () {
-  const defaults = {
-    largura: 300,
-    profundidade: 200,
-    altura: 150,
-    espessura: 3,
-    tipoMedida: "externa",
-    tipoCupula: "aberta",
-    tipoMontagem: "dentro",
-    viewMode: "montada",
-  };
+export const DEFAULT_STATE = {
+  largura: 300,
+  profundidade: 200,
+  altura: 150,
+  espessura: 3,
+  tipoMedida: "externa",
+  tipoCupula: "aberta",
+  tipoMontagem: "dentro",
+  viewMode: "montada",
+};
 
-  const state = { ...defaults };
-
-  window.CupulaState = {
-    defaults,
-    state,
-    set(partial) {
-      Object.assign(state, partial);
-      return { ...state };
-    },
-    reset() {
-      Object.assign(state, defaults);
-      return { ...state };
-    },
-    get() {
-      return { ...state };
-    },
-  };
-})();
+export function getDefaultState() {
+  return { ...DEFAULT_STATE };
+}
