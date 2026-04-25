dc6f1342a045e31d826ba159d77ea00733c80f3d
geazihenrique <geazi.henrique@gmail.com>
2026-04-24 21:57:50 -0300

    Corrige imports CDN do Three.js para o 3D no GitHub Pages

--- js/gerarModelo3D.js
+++ js/gerarModelo3D.js
@@ -1,5 +1,5 @@
-import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
-import { OrbitControls } from "https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js";
+import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js?module";
+import { OrbitControls } from "https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js?module";
 
 const EDGE = 0x4c1d95;
 const HIGHLIGHT = 0xc4b5fd;
